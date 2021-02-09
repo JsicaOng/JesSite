a4d0d0cf74ad590a8fb3e0662bd7940bcc5fe38c
JsicaOng <59130892+JsicaOng@users.noreply.github.com>
2021-02-09 17:13:32 +0800

    major updates
    updated contact & index
    created profile

---
+++ action.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+        var small={width: "50px",height: "50px"};
+        var large={width: "100px",height: "100px"};
+        var count=1;
+        $("#navbar-toggler").css(small).on('click',function () {
+            $("#mini_me").animate((count==1)?large:small);
+            count = 1-count;
+        });
+    });
+
+//const swim_path = {
+//    curviness: 1.25,
+//    autoRotate: true,
+//    values:[{
+//        x: 100, y:-20
+//    }]
+//};
+//const tween = new TimelineLite();
+//
+//tween.add(
+//    TweenLite.to(".dolphin", 1,{
+//        bezier: swim_path,
+//        ease: Power1.easeInOut
+//    })
+//);
